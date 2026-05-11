e3b5620641d0eb8dc9cdbbe3e29480ca2ae8810f
hanwoochang <0421kws@naver.com>
2026-05-11 14:40:08 +0900

    Create mailer.js

---
+++ ztna-policy-server/mailer.js
@@ -0,0 +1,13 @@
+//email 설정 인증
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+module.exports = transporter;
